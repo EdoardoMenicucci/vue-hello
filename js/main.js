3a14cd1783866a3d3b95b28face53f6bd23a93de
EdoardoMenicucci <edoardo.menicucci@libero.it>
2024-05-10 14:52:42 +0200

    vueJs <3

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            message: 'Hello Vue!',
+            chefigata: `vueJs e' na gran figata`
+        }
+    }
+}).mount('#app')
